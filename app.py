d545cb7616815942311fca9e76883de8757713aa
evabirami17 <142863672+evabirami17@users.noreply.github.com>
2023-09-12 15:28:27 +0530

    app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from tensorflow import keras
 model1 = keras.models.load_model('model/model.h5')
 
 
-def reserve(feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13,feature14,feature15,feature16,feature17):
-  prediction=model1.predict([[feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13,feature14,feature15,feature16,feature17]])
+def reserve(feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10):
+  prediction=model1.predict([[feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10]])
   print(prediction)
   return prediction
 
@@ -19,20 +19,14 @@ def main():
    feature1 = st.number_input("Feature 1", value=0.0)
    feature2 = st.number_input("Feature 2", value=0.0)
    feature3 = st.number_input("Feature 3", value=0.0)
-   feature4 = st.number_input("Feature 3", value=0.0)
-   feature5 = st.number_input("Feature 3", value=0.0)
-   feature6 = st.number_input("Feature 3", value=0.0)
-   feature7 = st.number_input("Feature 3", value=0.0)
-   feature8 = st.number_input("Feature 3", value=0.0)
-   feature9 = st.number_input("Feature 3", value=0.0)
-   feature10 = st.number_input("Feature 3", value=0.0)
-   feature11= st.number_input("Feature 3", value=0.0)
-   feature12= st.number_input("Feature 3", value=0.0)
-   feature13= st.number_input("Feature 3", value=0.0)
-   feature14= st.number_input("Feature 3", value=0.0)
-   feature15= st.number_input("Feature 3", value=0.0)
-   feature16= st.number_input("Feature 3", value=0.0)
-   feature17= st.number_input("Feature 3", value=0.0)
+   feature4 = st.number_input("Feature 4", value=0.0)
+   feature5 = st.number_input("Feature 5", value=0.0)
+   feature6 = st.number_input("Feature 6", value=0.0)
+   feature7 = st.number_input("Feature 7", value=0.0)
+   feature8 = st.number_input("Feature 8", value=0.0)
+   feature9 = st.number_input("Feature 9", value=0.0)
+   feature10 = st.number_input("Feature 10", value=0.0)
+ 
    result=''
    if st.button("Predict"):
     result=reserve(feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13,feature14,feature15,feature16,feature17)
